565e5cbb79255ab73d350b5783a6effa00a63ec3
danpung2 <danpung_2@naver.com>
2022-10-24 11:43:54 +0900

    카메라 각도 수정

--- index.js
+++ index.js
@@ -33,12 +33,19 @@ class App {
             0.1,
             100
         );
-        camera.position.z = 25;
+        // camera.position.z = 25;
+
+        camera.position.set(0, 25, 0);
+        camera.up.set(0, 0, 1);
+        camera.lookAt(0, 0, 0);
+
         this._camera = camera;
     }
 
     _setupLight(){
         const color = 0xffffff;
+        // const intensity = 3;
+        // const light = new THREE.PointLight(color, intensity);
         const intensity = 1;
         const light = new THREE.DirectionalLight(color, intensity);
         light.position.set(-1, 2, 4);
@@ -47,24 +54,24 @@ class App {
 
 
     _setupModel(){
-        const solarSystem = new THREE.Object3D();
-        this._scene.add(solarSystem);
+        const weSpace = new THREE.Object3D();
+        this._scene.add(weSpace);
 
         const radius = 1;
-        const widthSegments = 12;
-        const heightSegments = 12;
+        const widthSegments = 24;
+        const heightSegments = 24;
         const sphereGeometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
 
         const sunMaterial = new THREE.MeshPhongMaterial({
             emissive: 0xffff00, flatShading: true
         });
 
-        const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial);
-        sunMesh.scale.set(3, 3, 3);
-        solarSystem.add(sunMesh);
+        const centerMesh = new THREE.Mesh(sphereGeometry, sunMaterial);
+        centerMesh.scale.set(3, 3, 3);
+        weSpace.add(centerMesh);
 
         const earthOrbit = new THREE.Object3D();
-        solarSystem.add(earthOrbit);
+        weSpace.add(earthOrbit);
 
         const earthMaterial = new THREE.MeshPhongMaterial({
             color: 0x2233ff, emissive: 0x112244, flatShading: true
@@ -86,7 +93,7 @@ class App {
         moonMesh.scale.set(0.5, 0.5, 0.5);
         moonOrbit.add(moonMesh);
 
-        this._solarSystem = solarSystem;
+        this._weSpace = weSpace;
         this._earthOrbit = earthOrbit;
         this._moonOrbit = moonOrbit;
     }
@@ -110,7 +117,7 @@ class App {
     update(time){
         time *= 0.001
 
-        this._solarSystem.rotation.y = time / 2;
+        this._weSpace.rotation.y = time / 2;
         this._earthOrbit.rotation.y = time * 2;
         this._moonOrbit.rotation.y = time * 5;
     }
